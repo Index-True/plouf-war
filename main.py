ec11a8ff2470d34db3a00b1bb21fbec9d5a11256
Index_True <alix.biton@students.campus.academy>
2019-11-15 10:44:08 +0100

    Fixed #1 : Création du système de grilles

---
+++ main.py
@@ -0,0 +1,21 @@
+grids = [
+    [
+        [
+            0 for _ in range(10) # Generation d'une ligne contenant 10 `0`
+        ] for _ in range(10) # generation de 10 lignes
+    ], # Grille du joueur 0
+    [
+        [
+            0 for _ in range(10)
+        ] for _ in range(10)
+    ]  # Grille du joueur 1
+]
+"""
+Variable grids : contient l'etat de la grille de jeu pour les deux joueurs
+pour acceder a la case B4 du joueur 1 on ecrit : grids[0][1][3]
+                                                       |  |  |
+                                                  joueur  |  |
+                                                     colone  |
+                                                         ligne
+"""
+
