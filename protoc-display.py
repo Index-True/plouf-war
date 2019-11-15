5b0a7a712c674ca18a3b154c6ebecb600d0ea6a1
PaullyUlisses <55paully@gmail.com>
2019-11-15 13:35:05 +0100

    fixed #7 protoc-display.py edited
    harmonisation des paramètres avec le main + lire commentaires pour les nouveaux paramètres

--- protoc-display.py
+++ protoc-display.py
@@ -1,10 +1,18 @@
-def displayScreen(screen,viewer) :
+def displayScreen(grid,playerID,mode) :
+    """
+    Input:
+    gird = tableaux de donnés dans le tableau à trois dimensions
+    playerID = l'exact même que dans le main, id du joeur qui va regarder l'output
+    mode = boolean, True = le joueur regarde son tableau
+                    False= le joueur regarde le tableau de son adversaire
+    """
     disIn = [0,1,2,3,20,30,31,40,50]
-    if viewer :
-        disOut = [" ∙ "," o ","▒▒▒","▒▒▒","███","███","███","███","███"]
-    else :
-        disOut = [" ∙ "," o ","▒▒▒"," x "," ∙ "," ∙ "," ∙ "," ∙ "," ∙ "]
+    screen = grid[playerID]
 
+    if mode :
+        disOut = [" ∙ "," o ","▒▒▒","▒▒▒","███","███","███","███","███"]
+    else
+        disOut = [" ∙ "," o ","▒▒▒"," x "," ∙ "," ∙ "," ∙ "," ∙ "," ∙ "]
     for i in range(10):
         ln = ""
         for j in range(10):
