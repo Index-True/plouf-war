b82e921bb0512afbbec86d9fffd6e4cc519efe35
Index_True <alix.biton@students.campus.academy>
2019-11-15 16:18:27 +0100

    fixed casting bug

--- plouf-war/userInteractions.py
+++ plouf-war/userInteractions.py
@@ -13,11 +13,11 @@ def askCell(text):
 
     userInput = input(text)
 
-    while not userInput[0] in tab and not userInput[1:].isdigit() and not 1 <= userInput[1:] <= 10:
+    while not userInput[0] in tab and not userInput[1:].isdigit() and not 1 <= int(userInput[1:]) <= 10:
 
         userInput = input("Please enter values like A2 : ")
     
     col = tab.index(userInput[0])
-    line = userInput[1:]
+    line = int(userInput[1:])
     
     return line, col
