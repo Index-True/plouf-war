f9ac446700b0bb0f6ad380f0003d0b1196029a0a
Index_True <alix.biton@students.campus.academy>
2019-11-15 16:02:50 +0100

    Added Docstring to askCell

--- plouf-war/userInteractions.py
+++ plouf-war/userInteractions.py
@@ -1,4 +1,14 @@
 def askCell(text):
+    """
+    Fonction askCell, demande a l'utilisateur d'entrer une case, verifie les informations
+    entrées et retourne la case
+    # Paramètres
+    text : Le texte a afficher a l'utilisateur pour lui demander une case
+    # Return
+    retourne deux valeurs:
+    line : l'index de la ligne
+    col : l'index de la colone
+    """
     tab = ["A","B","C","D","E","F","G","H","I","J"]
 
     userInput = input(text)
