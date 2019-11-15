ae4857a7239f32299e0803485bb82e659cf22a3f
Alexis Jeanjean <alexis.wardmar@gmail.com>
2019-11-15 14:23:25 +0100

    ask cell a tester

--- posi.py
+++ posi.py
@@ -1,16 +1,26 @@
-def placeBoat(boatSize, boatId,grid):
+def askCell(text):
     tab = ["A","B","C","D","E","F","G","H","I","J"]
+    while not text[0] in tab and not text[1:].isdigit() and not 1 <= text[1:] <= 10:
+
+        text = input("Please enter values like A2 : ")
+    text = text[0]+', '+text[1:]
+    text = text.split(', ')
+    text[1]=int(text[1])
+    for i in range(len(tab)):
+        if text[0] == tab[i]:
+            text[0] = i
+
+    return(text)
+print(askCell("B8"))
+
+
+'''def placeBoat(boatSize, boatId,grid):
+    
     
     posiD = input("Enter the first cell of the table where you want to put your "+str(boatSize)+" cell boat ex:A1 :")
 
 #test si l'input est une chaine de 2 caractere, si le premier caractere est une lettre compris dans le tableau et le deuxieme caractere un nombre entre 1-10
-    while posiD != 1 or posiD[0] != (val for val in tab)or posiD[1]-1!= (j for j in range(len(tab))):
-
-        posiD = input("Please enter values like A2 : ")
-        
-        
-    for i in range(len(tab)):#replacer la lettre par un nombre
-        if posiD[0] ==tab[i]:
-            posiD[0] = i
-            
     
+            
+    direction = input("enter a direction ")''' 
+'''  '''
