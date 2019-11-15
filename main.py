697086ba0e1ab77643c6274a75f22b287212f45c
Index_True <alix.biton@students.campus.academy>
2019-11-15 10:56:56 +0100

    Advanced on scan #6 function added description added scan.stats variable

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 grids = [
     [
         [
-            0 for _ in range(10) # Generation d'une ligne contenant 10 `0`
-        ] for _ in range(10) # generation de 10 lignes
-    ], # Grille du joueur 0
+            0 for _ in range(10)  # Generation d'une ligne contenant 10 `0`
+        ] for _ in range(10)  # generation de 10 lignes
+    ],  # Grille du joueur 0
     [
         [
             0 for _ in range(10)
@@ -19,3 +19,26 @@ pour acceder a la case B4 du joueur 1 on ecrit : grids[0][1][3]
                                                          ligne
 """
 
+
+def scan(playerId):
+    """
+    # Description 
+        Fonction `scan(playerId)` permet de surveiller l'etat de la grille et d'informer 
+        la fonction parente si le joueur vient de couler un bateau ou si il vient de gagner.
+    ## Parmetres
+        playerId : L'id sur player dont il faut scanner la grille    
+    ## Retourne
+        Elle retourne un tuple `(shipKilled, hasWon)` de booleens, 
+        shipKilled est a true si un bateau viens d'etre coulé
+        hasWon est a true si le joueur viens de gagner
+    """
+    shipKilled, hasWon = False
+
+    return shipKilled, hasWon
+
+
+scan.stats = [
+    {
+        '50': True, '40': True, '30': True, '31': True, '20': True
+    } for i in range(2)
+]
